8dcc840fa5556534cb982d9ffc90f13207017248
Lednir <valsubaan@yahoo.com>
2025-09-07 22:06:46 +0800

    Added streamlit config files

---
+++ app/conf/constants.py
@@ -0,0 +1,4 @@
+APP_DESC = """
+[Intent Evaluator](https://github.com/LednirNaabus/intent-evaluator) is a modular Python pipeline that generates conversation data schemas based on a defined intent criteria.
+The Streamlit app provides a user-friendly, interactive interface to explore the pipeline’s output.
+"""
